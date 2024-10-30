8e1a89c938e3b366cc18581a16e4bf5f68f589f6
David Robert <contact@david-robert.fr>
2024-10-30 12:47:53 +0100

    php + html

--- index.php
+++ index.php
@@ -1,22 +1,31 @@
 
-<?php
+<?php $name = "David Robert"; ?>
 
-$age = 34;
-$isDead = false;
-
-//echo $age;
-
-if ($isDead === true) {
-    echo "Déjà mort";
-}
-
-
-
-function addNumbers($number1, $number2) {
-    return $number1 + $number2;
-}
-
-$result = addNumbers(10, 5);
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
 
 
-echo $result;
+<main>
+    <header>
+
+        <p>Bonjour <?php echo $name; ?></p>
+
+
+        <p>Veuillez vous connecter</p>
+    </header>
+
+    <h1>Le super blog de la piscine</h1>
+
+    <p>Découvrez les dernières actus !</p>
+</main>
+
+</body>
+</html>

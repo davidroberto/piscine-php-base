8a265b1b9bcb937b01eedf41ab7701c96a095a41
David Robert <contact@david-robert.fr>
2024-10-30 12:18:28 +0100

    function

--- index.php
+++ index.php
@@ -4,8 +4,19 @@
 $age = 34;
 $isDead = false;
 
-echo $age;
+//echo $age;
 
 if ($isDead === true) {
     echo "Déjà mort";
 }
+
+
+
+function addNumbers($number1, $number2) {
+    return $number1 + $number2;
+}
+
+$result = addNumbers(10, 5);
+
+
+echo $result;

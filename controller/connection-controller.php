2f1b865d18bb9584026366574e319d4d3d355795
David Robert <contact@david-robert.fr>
2024-11-04 14:18:42 +0100

    fix comments

--- controller/connection-controller.php
+++ controller/connection-controller.php
@@ -1,31 +1,38 @@
 <?php
 
+// j'initialise des variables me permettant de savoir
+// si le formulaire a été envoyé
+// et si les données envoyées sont correctes (identifiant et mot de passe ok)
+// ces variables sont utiles uniquement pour la view
 $isAuthenticated= false;
 $isFormSubmitted = false;
 
+
+// si le formulaire a été envoyé
 if ($_SERVER['REQUEST_METHOD'] === "POST") {
 
+    // je change la valeur de $isFormSubmitted
+    // pour la mettre à true
     $isFormSubmitted = true;
 
-    // vérifier en BDD que l'email existe et que le mot de passe correspond à l'email
+    // on vérifie les données envoyées
+    // si l'email correspond à celui attendu (dans une vrai app, on verifierait s'il l'email existre en BDD)
+    // et si le mot de passe est celui attendu
     if ($_POST['email'] === "contact.davidrobert@gmail.com" &&
         $_POST['password'] === "test"
     ) {
 
-        // démarre l'espace de stockage de session
+        // on démarre la session
+        // ça créé un espace de stockage spécifique pour un utilisateur
+        // lié à un identifiant unique généré
         session_start();
 
-        // variable superglobale, permettant d'accéder à la session
-        // de l'utilisateur
-        // ici je stocke une info permettant de vérifier si
-        // l'utilisateur est connecté ou non
+        // j'accède à l'espace de stockage de session de l'utilisateur
+        // avec la super globale $_SESSION
+        // et je créé une clé me permettant de savoir si l'utilisateur est connecté
         $_SESSION['is_authenticated'] = true;
 
-        // envoie au navigateur un id de session
-        // lié à l'espace de stockage
-        // cet id de session est stocké dans les cookies du navigateur
-
-
+        // je modifie la variable contenant l'info si l'utilisateur a réussi à se connecter ou non
         $isAuthenticated = true;
     }
 
